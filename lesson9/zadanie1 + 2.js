73e4f00eb829b51e3d9d41592174a74621fde6a5
michalrozek90 <michalrozek90@gmail.com>
2021-04-02 19:05:30 +0200

    exercise 1 & 2

---
+++ lesson9/zadanie1 + 2.js
@@ -0,0 +1,51 @@
+// ZADANIE 1
+
+function sayHello() {
+  console.log("Cześć Karol");
+}
+
+function add() {
+  console.log(2 + 2);
+}
+
+function minus() {
+  console.log(4 - 2);
+}
+
+function divide() {
+  console.log(8 / 2);
+}
+
+function multiple() {
+  console.log(2 * 4);
+}
+
+function modulo() {
+  console.log(8 % 3);
+}
+
+function power() {
+  console.log(2 ** 3);
+}
+
+function sum(firstNumber, secondNumber) {
+  return firstNumber + secondNumber;
+}
+
+// ZADANIE 2
+
+function power3(firstNumber) {
+  return firstNumber ** 3;
+}
+
+function multiple2(firstNumber, secondNumber) {
+  return firstNumber * secondNumber;
+}
+
+function changeMinutesForSeconds(firstNumber) {
+  return firstNumber * 60;
+}
+
+function sayHello(firstName) {
+  return "Cześć " + firstName;
+}
