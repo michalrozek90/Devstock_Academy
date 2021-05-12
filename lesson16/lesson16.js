4030d7d78e5eea29b1803322a6e7f24c285bca7e
michalrozek90 <michalrozek90@gmail.com>
2021-05-12 11:32:04 +0200

    add lessons 16, 17 & 18

---
+++ lesson16/lesson16.js
@@ -0,0 +1,32 @@
+// const string = "Hello";
+
+// for (property of string) {
+//   console.log(property);
+// }
+
+// const string2 = "Graci";
+
+// for (property in string2) {
+//   console.log(property);
+// }
+
+// const array = [1, 2, 3, 4, 5];
+
+// const newArray = array.map(function (item) {
+//   return item * 2;
+// });
+
+// console.log("array", array); // Wynik to "array", [1, 2, 3, 4, 5]
+// console.log("newArray", newArray); // Wynik to "newArray", [2, 4, 6, 8, 10]
+
+// const array = [3, 4, 1, 6, 2];
+
+// const newArray = array.sort(function (first, second) {
+//   return first - second;
+// });
+
+// console.log(newArray); // [1, 2, 3, 4, 6]
+
+// const str = ["ćma", "dom", "sevilla", "real madryt", "ąty"];
+
+// console.log(str.sort()); // wynik ["barca", "chelsea", "real madryt", "sevilla"]
