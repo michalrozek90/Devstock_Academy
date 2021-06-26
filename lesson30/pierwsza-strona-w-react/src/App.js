866adc463f2a13498da4e2634ed783ad9f436b8f
michalrozek90 <michalrozek90@gmail.com>
2021-06-26 12:01:55 +0200

    add React lesson

---
+++ lesson30/pierwsza-strona-w-react/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Title from "./title";
+
+function App() {
+  return (
+    <div className="App">
+      <Title title="losowy tytuł" />
+    </div>
+  );
+}
+
+export default App;
